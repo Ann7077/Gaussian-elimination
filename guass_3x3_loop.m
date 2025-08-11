727d1f099ee193f8aaf37f0c20ccbdfe17199750
ann <anyu2021yang@gmail.com>
2025-08-11 14:27:30 -0700

    gauss_3x3_loop.m half finished, correct

--- guass_3x3_loop.m
+++ guass_3x3_loop.m
@@ -11,13 +11,21 @@ for n = 1:1:(r-1)
     b(n) = b(n)/a(n,n);
     a(n,:) = a(n,:)/a(n,n);
     for m = (n+1):1:r
-        b(n+1) = b(n+1) - [a(n+1,n)/a(n,n)]*b(n);
-        a(n+1,:) = a(n+1,:) - [a(n+1,n)/a(n,n)]*a(n,:);
+        b(m) = b(m) - [a(m,n)/a(n,n)]*b(n);
+        a(m,:) = a(m,:) - [a(m,n)/a(n,n)]*a(n,:);
     end
 end
 
 %x3 = b(r)/a(r,r);
 %for n = (r-1):-1:1
-    
 
+% solution
+x3 = b(3)/a(3,3);
+x2 = b(2) - a(2,3)*x3;
+x1 = b(1) - a(1,2)*x2 - a(1,3)*x3;
+
+% print solution
+fprintf("x1 = %d\n", x1);
+fprintf("x2 = %d\n", x2);
+fprintf("x3 = %d\n", x3);
         
