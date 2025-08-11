f62370631c648816fca53a18cd6fd87827306930
ann <anyu2021yang@gmail.com>
2025-08-10 22:23:53 -0700

    comment added to gauss_2x2.m

--- gauss_2x2.m
+++ gauss_2x2.m
@@ -3,14 +3,16 @@ clear all;
 a = [2 3; 4 5];
 b = [6; 8];
 
-ref = a\b;
-% solution is x1 = -3, x2 = 4
+ref = a\b; 
+  % This is the value of x1 and x2 calculated by MATLAB's build in function
 
 % first row
 b(1) = b(1)/a(1,1);        
 % a(1,2) = a(1,2)/a(1,1);
 % a(1,1) = a(1,1)/a(1,1);   
-a(1, :) = a(1, :)/a(1,1);
+a(1, :) = a(1, :)/a(1,1); 
+  % This line can replace the previous code I wrote, it divided each
+  % element in row 1 of a by the value of a(1,1)
 
 % second row
 b(2) = b(2)-a(2,1)*b(1);
