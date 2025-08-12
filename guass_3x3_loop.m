358cbf7fb32154d4170aa935565e1276ce8329e5
ann <anyu2021yang@gmail.com>
2025-08-11 17:17:38 -0700

    gauss_3x3_loop.m complete, correct

--- guass_3x3_loop.m
+++ guass_3x3_loop.m
@@ -3,10 +3,14 @@ clear all;
 a = [5 2 3; 4 7 6; 8 9 10];
 b = [28; 53; 83];
 
+%a = [2 3 4; 5 6 7; 7 9 11];
+%b = [13; 32; 45];
+
 ref = a\b
 
 r = 3;
 
+% solve
 for n = 1:1:(r-1)
     b(n) = b(n)/a(n,n);
     a(n,:) = a(n,:)/a(n,n);
@@ -16,16 +20,53 @@ for n = 1:1:(r-1)
     end
 end
 
-%x3 = b(r)/a(r,r);
-%for n = (r-1):-1:1
-
 % solution
+arr = zeros(1,[]);
+arr(r) = b(r)/a(r,r);
+var1 = ['x' num2str(r)];
+fprintf("%s = %d\n",[var1], arr(r));
+
+for i = (r-1):-1:1
+    arr(i) = [b(i) - a(i,i+1)*arr(i+1)];
+    j = i+2;
+    while j <= r
+        temp = -a(i,j)*arr(j);
+        arr(i) = arr(i) + temp;
+        j = j+1;
+    end
+    var2 = ['x' num2str(i)];
+    fprintf("%s = %d\n",[var2], arr(i));
+end
+
+%{
+x5 = b(5)/a(5,5);
+x4 = b(4) - a(4,5)*x5;
+x3 = b(3) - a(3,4)*x4 - a(3,5)*x5;
+x2 = b(2) - a(2,3)*x3 - a(2,4)*x4 - a(2,5)*x5;
+x1 = b(1) - a(1,2)*x2 - a(1,3)*x3 - a(1,4)*x4 - a(1,5)*x5;
+%}
+
+%{
+var1 = ['x' num2str(r)];
+fprintf("%s = %d\n",[var1], [b(r)/a(r,r)]);
+
+for i = (r-1):-1:1
+    var2 = ['x' num2str(i)];
+    temp = [b(i) - a(i,1+1)*var1]
+    fprintf("%s = %d\n",[var2], temp);
+end
+%}
+
+
+%{
+% solution (hard code)
 x3 = b(3)/a(3,3);
 x2 = b(2) - a(2,3)*x3;
 x1 = b(1) - a(1,2)*x2 - a(1,3)*x3;
 
-% print solution
+% print solution (hard code)
 fprintf("x1 = %d\n", x1);
 fprintf("x2 = %d\n", x2);
 fprintf("x3 = %d\n", x3);
+%}
         
